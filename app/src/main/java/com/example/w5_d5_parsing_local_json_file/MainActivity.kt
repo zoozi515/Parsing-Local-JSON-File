5e048ad4a4d1be15e63a69027148beedf8190331
Zahra Yousef <45607455+zoozi515@users.noreply.github.com>
2021-10-24 02:52:25 +0300

    Parsing Local JSON File - Version #1

---
+++ app/src/main/java/com/example/w5_d5_parsing_local_json_file/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.w5_d5_parsing_local_json_file
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
